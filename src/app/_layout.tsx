e719b0140a556c8d12f90eb1f50a29e9987d4e7e
João Rodrigues <joaocdvrodrigues@gmail.com>
2023-07-12 11:02:15 +0100

    chore: move /app inside the /src directory

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -24,7 +24,7 @@ StatusBar.setHidden(true)
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
   })
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
