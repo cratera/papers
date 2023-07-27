a82fa2a2b0541e16e9eba47204de885a42097a92
João Rodrigues <joaocdvrodrigues@gmail.com>
2023-07-27 22:58:15 +0100

    chore: bump dependencies

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,29 +1,26 @@
 import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds'
-import { Preview } from '@storybook/react'
+import { Decorator, Parameters } from '@storybook/react'
 import React from 'react'
 import { View } from 'react-native'
 
-const preview: Preview = {
-  decorators: [
-    (StoryFn) => (
-      <View>
-        <StoryFn />
-      </View>
-    ),
-    withBackgrounds,
-  ],
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-    backgrounds: {
-      default: 'plain',
-      values: [{ name: 'plain', value: 'white' }],
+export const decorators = [
+  (StoryFn) => (
+    <View>
+      <StoryFn />
+    </View>
+  ),
+  withBackgrounds,
+] satisfies Decorator[]
+
+export const parameters = {
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
-}
-
-export default preview
+  backgrounds: {
+    default: 'plain',
+    values: [{ name: 'plain', value: 'white' }],
+  },
+} satisfies Parameters
